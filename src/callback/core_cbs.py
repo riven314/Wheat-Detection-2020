46a86da3e5ae2cafa91c8d01c38a05a4632419f2
riven314 <alexlauwh@gmail.com>
2020-05-18 21:23:03 +0800

    created a jupyter notebook for a deeper look on RetinaNet

--- src/callback/core_cbs.py
+++ src/callback/core_cbs.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import torch
 
 from fastai2.vision.all import AvgMetric
+from fastai2.vision.all import Callback
 
 
 class RetinaNetCallback(Callback):
--- src/main.py
+++ src/main.py
@@ -2,18 +2,18 @@
 import os
 from functools import partial
 from pathlib import Path
+
 import warnings
 warnings.filterwarnings('ignore')
 
 import torch
+from fastai2.vision.all import Learner
 
 from src.data.dls import build_dataloaders
-from src.model.FasterRCNN import get_faster_rcnn, split_faster_rcnn_params
-from src.metrics.loss import WeightedMultiLoss
-from src.metrics.mAP import mAP
-from src.callback.core_cbs import FasterRCNNCallback, CheckpointCallback
-from src.callback.core_cbs import after_batch, begin_validate
-from src.callback.learner import WheatLearner
+from src.model.model import get_retinanet, split_param_groups
+from src.metrics.loss import get_retinanet_loss
+from src.callback.core_cbs import CheckpointCallback
+#from src.metrics.mAP import mAP
 
 
 DATA_PATH = Path('/userhome/34/h3509807/wheat-data')
@@ -22,43 +22,42 @@ RESIZE_SZ = 256
 TEST_MODE = False
 RAND_SEED = None
 
-BS = 8 # 16 backbone fine-tune OOM
-INIT_LR = 2e-4
+BS = 32
+INIT_LR = 1e-4
 INIT_EPOCH = 20
 
 IS_FT = True
-FT_LR = slice(2e-6, 1e-4)
+FT_LR = slice(2e-7, 5e-5)
 FT_EPOCH = 20
+
 
 get_dls = partial(build_dataloaders, data_path = DATA_PATH, 
                   resize_sz = RESIZE_SZ, norm = False, 
                   rand_seed = RAND_SEED, test_mode = TEST_MODE)
 dls = get_dls(bs = BS)
-model = get_faster_rcnn()
 
-multi_loss = WeightedMultiLoss()
-thresholds = [i for i in map(lambda i: i/100, range(50, 80, 5))]
-mAP_calc = partial(mAP, thresholds = thresholds)
 
-faster_rcnn_cb = FasterRCNNCallback(img_size = RESIZE_SZ)
+model = get_retinanet()
+retinanet_loss = get_retinanet_loss(ratios = None, scales = None)
+#thresholds = [i for i in map(lambda i: i/100, range(50, 80, 5))]
+#mAP_calc = partial(mAP, thresholds = thresholds)
+
 save_cb = CheckpointCallback(SAVE_DIR)
-
-learn = WheatLearner(dls, model, loss_func = multi_loss, 
-                     splitter = split_faster_rcnn_params,
-                     metrics = mAP_calc,
-                     cbs = [faster_rcnn_cb, save_cb])
-                     #metrics = map_getter,
+learn = Learner(dls, model, 
+                loss_func = retinanet_loss, 
+                splitter = split_param_groups,
+                cbs = [save_cb])
+                     #metrics = mAP_calc,
 
 learn.freeze()
 learn.fit_one_cycle(INIT_EPOCH, INIT_LR)
 if IS_FT:
-    ft_bs = int(BS/2)
-    learn.dls = get_dls(bs = ft_bs)
+    learn.dls = get_dls(bs = BS)
     learn.unfreeze()
     learn.fit_one_cycle(FT_EPOCH, FT_LR)
 
-model_path = SAVE_DIR / 'final_model.pth'
-learn.save('final_learner')
+model_path = SAVE_DIR / 'final_retinanet_model.pth'
+learn.save('final_retinanet_learner')
 torch.save(learn.model.state_dict(), model_path)
 print('final checkpoints saved')
 
